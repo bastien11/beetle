0861e8b97ffada367f554dafeb2c528a9202260c
SCHERRER Bastien <sb410167@s202p11m.iutnice.unice.fr>
2017-06-09 14:14:37 +0200

    added game elements

---
+++ beetle/GameElements.swift
@@ -0,0 +1,20 @@
+//
+//  GameElements.swift
+//  Beetle
+//
+//  Created by SCHERRER Bastien on 09/06/2017.
+//  Copyright © 2017 bastael. All rights reserved.
+//
+
+import SpriteKit
+
+struct CollisionBitMask {
+    static let birdCategory:UInt32 = 0x1 << 0
+    static let pillarCategory:UInt32 = 0x1 << 1
+    static let flowerCategory:UInt32 = 0x1 << 2
+    static let groundCategory:UInt32 = 0x1 << 3
+}
+
+extension GameScene{
+    
+}
