84611581ff85f4b033def62c981a69ef927e7a83
SCHERRER Bastien <sb410167@s202p11m.iutnice.unice.fr>
2017-06-09 14:28:12 +0200

    oops

--- beetle/GameScene.swift
+++ beetle/GameScene.swift
@@ -35,32 +35,6 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        
-        1
-        2
-        3
-        4
-        5
-        6
-        7
-        8
-        9
-        10
-        11
-        12
-        13
-        14
-        15
-        16
-        17
-        18
-        19
-        20
-        21
-        22
-        23
-        24
         if isGameStarted == false{
             //1
             isGameStarted =  true
