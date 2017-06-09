9ee7d0827c128e2994441bcc22a04a86f18f9f4b
SCHERRER Bastien <sb410167@s202p11m.iutnice.unice.fr>
2017-06-09 14:20:37 +0200

    GameScene update

--- beetle/GameScene.swift
+++ beetle/GameScene.swift
@@ -31,7 +31,7 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
     var repeatActionBird = SKAction()
     
     override func didMove(to view: SKView) {
-        
+        createScene()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -39,7 +39,19 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        /// Called before each frame is rendered
+        if isGameStarted == true{
+            if isDied == false{
+                enumerateChildNodes(withName: "background", using: ({
+                    (node, error) in
+                    let bg = node as! SKSpriteNode
+                    bg.position = CGPoint(x: bg.position.x - 2, y: bg.position.y)
+                    if bg.position.x <= -bg.size.width {
+                        bg.position = CGPoint(x:bg.position.x + bg.size.width * 2, y:bg.position.y)
+                    }
+                }))
+            }
+        }
     }
     
     func createScene() {
@@ -52,5 +64,18 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
         
         self.physicsWorld.contactDelegate = self
         self.backgroundColor = SKColor(red: 80.0/255.0, green: 192.0/255.0, blue: 203.0/255.0, alpha: 1.0)
+        for i in 0..<2
+        {
+            let background = SKSpriteNode(imageNamed: "bg")
+            background.anchorPoint = CGPoint.init(x: 0, y: 0)
+            background.position = CGPoint(x:CGFloat(i) * self.frame.width, y:0)
+            background.name = "background"
+            background.size = (self.view?.bounds.size)!
+            self.addChild(background)
+        }
+        birdSprites.append(birdAtlas.textureNamed("bird1"))
+        birdSprites.append(birdAtlas.textureNamed("bird2"))
+        birdSprites.append(birdAtlas.textureNamed("bird3"))
+        birdSprites.append(birdAtlas.textureNamed("bird4"))
     }
 }
